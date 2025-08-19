d1dc199c108ecc6eee34069cf460355c9e72b98a
Alex Dziarnowski <alexdziarn@gmail.com>
2025-08-19 18:40:06 -0400

    add discord

--- server/app.ts
+++ server/app.ts
@@ -12,12 +12,12 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(TOKEN || '');
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+  await rest.put(Routes.applicationCommands(CLIENT_ID || ''), { body: commands });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
